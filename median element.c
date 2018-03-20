c496a74141beef1b02f8f0cd1c02976490e558b9
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-03-20 10:34:41 +0000

    Create median element.c

---
+++ median element.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a[100],t,n,i,j;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+printf("%d",a[n/2]);
+return 0;
+}
